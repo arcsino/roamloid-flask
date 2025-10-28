8cf79d9f657a9b6652fab08913368ff4c3c50727
arcsino <173187432+arcsino@users.noreply.github.com>
2025-10-28 11:24:00 +0900

    CORS設定を一時的に全オリジンに許可

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from flask_socketio import SocketIO
 
 
 app = create_app()
-socketio.init_app(app, cors_allowed_origins=settings.CORS_ORIGINS)
+# socketio.init_app(app, cors_allowed_origins=settings.CORS_ORIGINS)
+socketio.init_app(app, cors_allowed_origins="*")
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000, debug=True)
